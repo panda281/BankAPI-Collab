2be3af616c561dd4b3b45c42b16777a83233f4bf
Abinet <abinettamiru28@gmail.com>
2023-11-23 18:30:52 +0300

    swagger added

--- src/main/java/com/gebeya/bankapi/BankApiApplication.java
+++ src/main/java/com/gebeya/bankapi/BankApiApplication.java
@@ -1,9 +1,14 @@
 package com.gebeya.bankapi;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+//@EnableScheduling
+@OpenAPIDefinition(info = @Info(title = "Bank API", version = "2.0", description = "Bank API"))
 public class BankApiApplication {
 
 	public static void main(String[] args) {
