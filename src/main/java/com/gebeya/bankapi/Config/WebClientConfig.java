f278b154ef696feec3d68737486db21b209d084c
Elsa <hagoselizabeth06@gmail.com>
2023-11-23 12:25:49 -0800

    messages

---
+++ src/main/java/com/gebeya/bankapi/Config/WebClientConfig.java
@@ -0,0 +1,26 @@
+
+package com.gebeya.bankAPI.Config;
+
+        import org.springframework.context.annotation.Bean;
+        import org.springframework.context.annotation.Configuration;
+        import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+    @Bean
+    public WebClient.Builder webClientBuilder(){
+        return WebClient.builder();
+    }
+
+    @Bean
+    public WebClient webClientForOtp(WebClient.Builder webClientBuilder)
+    {
+        return webClientBuilder.baseUrl("https://sms.yegara.com/api2").build();
+    }
+
+    @Bean
+    public WebClient webClientForTopUp(WebClient.Builder webClientBuilder)
+    {
+        return webClientBuilder.baseUrl("http://192.168.1.43:9090/mtelecom").build();
+    }
+}
