fca0bbe2a05911b68847dae366ebc32836bd2823
Elsa <hagoselizabeth06@gmail.com>
2023-11-23 11:01:33 -0800

    .

---
+++ src/main/java/com/gebeya/bankapi/Config/LocaleConfig.java
@@ -0,0 +1,29 @@
+package com.gebeya.bankAPI.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
+
+@Configuration
+public class LocaleConfig {
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver sir = new SessionLocaleResolver();
+        sir.setDefaultLocale(Locale.US);
+        return sir;
+    }
+    @Bean
+    public ResourceBundleMessageSource messageSource()
+
+    {
+        ResourceBundleMessageSource rs = new ResourceBundleMessageSource();
+        rs.setBasename("messages");
+        rs.setUseCodeAsDefaultMessage(true);
+        return rs;
+    }
+
+}
