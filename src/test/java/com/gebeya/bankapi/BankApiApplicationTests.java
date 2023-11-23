c0152ca86dedbe6eaf0ff2b1f5d563267a811e0c
Abinet <abinettamiru28@gmail.com>
2023-11-23 18:03:50 +0300

    inital commit

---
+++ src/test/java/com/gebeya/bankapi/BankApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gebeya.bankapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
